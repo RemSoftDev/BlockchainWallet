af782de533f203100bf05a8bbcbff5d75a61b949
Andrew Samoydyuk <andrewsamoydyuk@gmail.com>
2018-06-27 13:04:17 +0300

    update design : add navbar, footer

---
+++ Wallet/ClientApp/src/app/footer-menu/footer-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-menu',
+  templateUrl: './footer-menu.component.html',
+  styleUrls: ['./footer-menu.component.css']
+})
+export class FooterMenuComponent {
+  public ethAddress = "0x281055afc982d96fab65b3a49cac8b878184cb16";
+  public btcAddress = "0x281055afc982d96fab65b3a49cac8b878184cb16";
+}
