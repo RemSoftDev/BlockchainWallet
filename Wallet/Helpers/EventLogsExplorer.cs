26e70b284a571f0bb22d1744e857917cd3dc167e
AndrewSamoydyuk <andrewsamoydyuk@gmail.com>
2018-09-04 18:11:14 +0300

    improve get token holders by date

--- Wallet/Helpers/EventLogsExplorer.cs
+++ Wallet/Helpers/EventLogsExplorer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Wallet.Models;
 
 namespace Wallet.Helpers
@@ -9,9 +10,7 @@ namespace Wallet.Helpers
     {
         public static List<TokenHolder> GetInfoFromLogs(List<CustomEventLog> logs)
         {
-            List<TokenHolder> holders = new List<TokenHolder>();
-
-            var addresses = new HashSet<string>();
+            var addresses = new List<string>();
 
             logs.ForEach(l =>
             {
@@ -19,18 +18,37 @@ namespace Wallet.Helpers
                 addresses.Add(l.To);
             });
 
-            foreach (var address in addresses)
-            {
-                var generalTransNumber = logs.Count(e => e.To.Equals(address,
-                                                             StringComparison.CurrentCultureIgnoreCase) ||
-                                                         e.From.Equals(address,
-                                                             StringComparison.CurrentCultureIgnoreCase));
+            addresses = addresses.Distinct().ToList();
 
+            var tasks = new List<Task<List<TokenHolder>>>();
+
+            for (int i = 0; i < addresses.Count; i += 1000)
+            {
+                List<string> temp = addresses.Skip(i).Take(1000).ToList();
+                var task = Task.Run(() => SortHolders(temp, logs));
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            var result = new List<TokenHolder>();
+            tasks.ForEach(r => result.AddRange(r.Result));
+
+            return result;
+        }
+
+        private static async Task<List<TokenHolder>> SortHolders(List<string> addresses, List<CustomEventLog> logs)
+        {
+            var holders = new List<TokenHolder>();
+
+            foreach (var address in addresses.AsQueryable())
+            {
                 var sentTransactionsNumber =
                     logs.Count(e => e.From.Equals(address, StringComparison.CurrentCultureIgnoreCase));
 
                 var recievedTransactionsNumber =
                     logs.Count(e => e.To.Equals(address, StringComparison.CurrentCultureIgnoreCase));
+
+                var generalTransNumber = sentTransactionsNumber + recievedTransactionsNumber;
 
                 var tokenSentNumber = logs.Where(e => e.From.Equals(address, StringComparison.CurrentCultureIgnoreCase))
                     .Select(t => t.AmountOfToken).Sum();
